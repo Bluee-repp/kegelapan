ba82fd03cfddf213764b1fb8cd69cdbb4c5550d3
hyunkk-ark <hyunkkcahyunkk@gmail.com>
2025-09-13 17:35:12 +0800

    permohonan maaf

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Bisa dipakai buat interaktif nanti
 console.log("Portfolio Website Loaded!");
+
+const toggleBtn = document.getElementById("darkModeToggle");
+
+if (toggleBtn) {
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // ubah teks tombol sesuai mode
+    if (document.body.classList.contains("dark-mode")) {
+      toggleBtn.textContent = "☀️ Mode Terang";
+    } else {
+      toggleBtn.textContent = "🌙 Mode Gelap";
+    }
+  });
+}
